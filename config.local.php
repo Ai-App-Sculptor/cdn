9611d9220a1bc7d8524db5b124d31d4240610e4f
Anwar Elsayed <anwar.elsayed@check24.de>
2025-08-22 11:33:39 +0200

    Add the script

---
+++ config.local.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Local configuration file - NOT tracked by git
+ * Contains sensitive or environment-specific settings
+ */
+
+return [
+    'api' => [
+        'finnhub_logo_base_url' => 'https://static2.finnhub.io/file/publicdatany/finnhubimage/stock_logo/',
+        'stock_status_api_url' => 'https://codestomp.com/api/stock-status'
+    ]
+];
